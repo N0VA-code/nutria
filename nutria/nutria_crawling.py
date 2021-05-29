11b5b877653c4833e8a26121315fd60b44b3e6ed
seokzoo <itwhtjrwn01@gmail.com>
2021-05-29 15:00:15 +0900

    getListOfDish implementation

--- nutria/nutria_crawling.py
+++ nutria/nutria_crawling.py
@@ -18,9 +18,18 @@ def getListOfDish(q, pg=0):
     summary = html.select_one('div.searchResultSummary')
     if summary == None:
         return None
-    #print(summary.text)
+    ss = ''.join(x for x in summary.text if x not in '\r\t\n').split(' ')
+    numberOfItems = int(ss[0][:-1])
+    viewItems = int(ss[2])
+    if numberOfItems < 10:
+        numberOfItems = 1
+        viewItems = 1
+    elif numberOfItems / viewItems != 0:
+        numberOfItems += 10
+    pages = int(numberOfItems / viewItems)
     for e in html.select('table.generic.searchResult td'):
         url = domainUrl + e.select_one('a')['href']
         nameOfDish = e.select_one('a').text
         listOfDish.append({'nameOfDish' : nameOfDish, 'url' : url})
-    return listOfDish
+    r = {'pages' : pages, 'listOfDish' : listOfDish}
+    return r
